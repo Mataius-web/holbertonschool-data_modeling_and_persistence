85d35bc963ae372fd6deb04b73ba0dbe66715a1a
mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-04 16:44:06 +0200

    add 1st task

--- sql_join/1-books-with-authors.sql
+++ sql_join/1-books-with-authors.sql
@@ -1,3 +1,5 @@
-SELECT name
-FROM books INNER JOIN authors ON books.author_id = authors.id
-ORDER BY title ASC
+SELECT books.title, authors.name AS author_name
+FROM books
+INNER JOIN authors
+ON books.author_id = authors.id
+ORDER BY books.title ASC;
