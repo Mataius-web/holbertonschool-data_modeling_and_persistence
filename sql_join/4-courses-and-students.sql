926e5d3dffb65ef7cc0b40ee135fd117d4d013c9
mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-04 16:47:42 +0200

    Add 4th task

---
+++ sql_join/4-courses-and-students.sql
@@ -0,0 +1,7 @@
+SELECT courses.title AS course_title, students.name AS student_name
+FROM courses
+LEFT JOIN enrollments
+ON enrollments.course_id = courses.id
+LEFT JOIN students
+ON enrollments.student_id = students.id
+ORDER BY courses.title ASC, students.name ASC;
