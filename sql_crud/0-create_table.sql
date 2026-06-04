0e4f275ef26276c0df0adb9bb8ea8dfd1c6566d0
mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-04 15:37:29 +0200

    Add README + 1st task

---
+++ sql_crud/0-create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books (
+    id INTEGER NOT NULL,
+    title TEXT NO NULL,
+    author TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL,
+    published_year INTEGER NOT NULL
+);
