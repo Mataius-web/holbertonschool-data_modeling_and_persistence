1bd58e67a5c1aab3b718fc558ace9137d7eaf007
mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-04 16:50:41 +0200

    Add 7th task

---
+++ sql_join/7-course-enrollment-count.sql
@@ -0,0 +1,6 @@
+SELECT courses.title AS course_title, COUNT(enrollments.student_id) AS enrollment_count
+FROM courses
+LEFT JOIN enrollments
+ON enrollments.course_id = courses.id
+GROUP BY courses.id, courses.title
+ORDER BY enrollment_count DESC, courses.title ASC;
